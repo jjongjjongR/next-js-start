8d81accc5f2ebc710ded584f9924de3bfc1d5b1a
이종헌 <jongheon@ijongheons-MacBook-Air.local>
2024-07-21 21:26:04 +0900

    first commit

---
+++ app/about-us/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'About us | Next Movies',
+  }
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h1>About us!</h1>
+        </div>
+    );
+}
